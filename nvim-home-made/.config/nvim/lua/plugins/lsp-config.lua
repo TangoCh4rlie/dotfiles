2246a925a597521aaaeee186e23474638555fec8
TangoCh4rlie <elouanreymond@tutanota.com>
2025-03-22 17:14:41 +0100

    lspconfig

--- nvim-home-made/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim-home-made/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,56 +1,63 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
-			local lspconfig = require("lspconfig")
+      local lspconfig = require("lspconfig")
 
-			lspconfig.lua_ls.setup({
+      lspconfig.lua_ls.setup({
         capabilities = capabilities
       })
-			lspconfig.ts_ls.setup({
+      lspconfig.ts_ls.setup({
         capabilities = capabilities
       })
       lspconfig.html.setup({
         capabilities = capabilities
       })
       lspconfig.tailwindcss.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
+        init_options = {
+          userLanguages = {
+            elixir = "html-eex",
+            eelixir = "html-eex",
+            heex = "html-eex",
+          },
+        },
       })
       lspconfig.elixirls.setup({
-        cmd = {"/home/tangocharlie/.local/share/nvim/mason/packages/elixir-ls/language_server.sh"},
+        cmd = { "/home/tangocharlie/.local/share/nvim/mason/packages/elixir-ls/language_server.sh" },
         capabilities = capabilities
       })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 
-			vim.diagnostic.config({
-				signs = {
-					text = {
-						[vim.diagnostic.severity.ERROR] = "✘",
-						[vim.diagnostic.severity.WARN] = "▲",
-						[vim.diagnostic.severity.HINT] = "⚑",
-						[vim.diagnostic.severity.INFO] = "»",
-					},
-				},
-			})
-		end,
-	},
+      vim.diagnostic.config({
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = "✘",
+            [vim.diagnostic.severity.WARN] = "▲",
+            [vim.diagnostic.severity.HINT] = "⚑",
+            [vim.diagnostic.severity.INFO] = "»",
+          },
+        },
+      })
+    end,
+  },
 }
